eebdb9b906ce84a7d281507c712836f8b5134bdb
Magier012 <ernst.tim04@gmail.com>
2026-01-02 21:25:03 +0100

    fix DeviceWebController
    dwqdadasdasdadaw

--- src/main/java/sys/bac/adapters/in/api/adapter/device/DeviceWebController.java
+++ src/main/java/sys/bac/adapters/in/api/adapter/device/DeviceWebController.java
@@ -137,6 +137,7 @@ public class DeviceWebController {
         DeviceDTO result = dSA.createDevice(device);
         
         return Response.status(Response.Status.CREATED)
+        .cacheControl(noStore())
         .header("Location", new Link(Link.devices.getHref() + "/" + result.getId(), "getDevice", "application/json").getHeaderLink(uri))
         .build();
     }
@@ -147,6 +148,7 @@ public class DeviceWebController {
     public Response updateDevice(@Positive @PathParam("id") long id, @Valid DeviceDTO device) {
         dSA.updateDevice(id, device);
         return Response.noContent()
+        .cacheControl(noStore())
         .header("Link", new Link(Link.devices.getHref() + "/" + id, "getDevice", "application/json").getHeaderLink(uri))
         .build();
     }
@@ -156,6 +158,7 @@ public class DeviceWebController {
     public Response deleteDevice(@Positive @PathParam("id") long id) {
         dSA.deleteDevice(id);
         return Response.noContent()
+        .cacheControl(noStore())
         .header("Link", Link.devices.getHeaderLink(uri))
         .build();
     }
